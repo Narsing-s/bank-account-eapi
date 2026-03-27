f09866966ccdcc9a7a0120d9311fc3423c2c8862
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-03-28 04:04:57 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // config.js
 window.AppConfig = {
-  API_BASE: "https://bank-account-eapi-jik9pb.5sc6y6-4.usa-e2.cloudhub.io"
+  API_BASE: "https://bank-account-eapi-jik9pb.5sc6y6-4.usa-e2.cloudhub.io/api"
 };
